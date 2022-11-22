d1da593d2ab88d5a8ecd0cf182cb4242af9da6c2
apache0505 <jiayulutsk@ukr.net>
2022-11-22 12:27:25 +0200

    wq

--- 04_modules_using_libraries/task_01/index.js
+++ 04_modules_using_libraries/task_01/index.js
@@ -5,6 +5,12 @@
 // импортируйте ф-цию fetchUser из profile/gateway.js под таким же именем
 // импортируйте ф-цию printProfile из profile/index.js под таким же именем
 
+import calcExpression, { sum, mult } from './calculator/index.js';
+import fetchUser from './profile/gateway.js';
+import { printProfile } from './profile/index.js';
+import './polyfills/array-flat.js';
+import './polyfills/array-flatMap.js';
+
 const calcResult = calcExpression('1 + 2');
 const sumResult = sum(1, 2);
 const multResult = mult(1, 2);
